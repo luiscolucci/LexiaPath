60753604ca7e6125497d94ebe7521d34e91fd1ca
luiscolucci <colucci.luis@gmail.com>
2025-11-21 16:32:01 -0300

    LexiaPath-Dislexia

--- backend/teste-modelos-api.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-# Cole sua chave aqui
-genai.configure(api_key="AIzaSyAnpi3KYGvHy48hxgnprZqwnYc-EsAuZTQ")
-
-print("Listando modelos disponíveis...")
-for m in genai.list_models():
-  if 'generateContent' in m.supported_generation_methods:
-    print(m.name)
